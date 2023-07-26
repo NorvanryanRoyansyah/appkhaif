a3f730861a5f34490c5d5e210969a1d7be43904a
Norvanryan Royansyah <85242156+NorvanryanRoyansyah@users.noreply.github.com>
2023-07-26 18:56:52 +0700

    Update welcome.php

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -4,7 +4,4 @@
 <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 <?php echo $this->session->flashdata('message')?>
 </div>
-    <section class="content">
-        <p class="text-center"><marquee width="600" height="40"><b>SELAMAT DATANG DI FOTOCOPY AL KHAIF KUALA KAPUAS</b></marquee></p>
-    </section>
 </div>
